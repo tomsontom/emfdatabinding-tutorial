7fe855d615e433e6d8732dc6c2244da2e396deb6
Tom Schindl <tom.schindl@bestsolution.at>
2011-04-30 18:19:05 +0200

    adding emf databinding fork

---
+++ org.eclipse.emf.databinding/src/org/eclipse/emf/databinding/IEMFListProperty.java
@@ -0,0 +1,167 @@
+/**
+ * <copyright> 
+ *
+ * Copyright (c) 2008 Matthew Hall and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   Matthew Hall - initial API and implementation (bug 195222)
+ *   Tom Schindl <tom.schindl@bestsolution.at> - port to EMF in 262160
+ * </copyright>
+ *
+ * $Id: IEMFListProperty.java,v 1.3 2011/04/22 12:56:28 tschindl Exp $
+ */
+package org.eclipse.emf.databinding;
+
+import java.util.List;
+
+import org.eclipse.core.databinding.property.list.IListProperty;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+
+/**
+ * <p><b>PROVISIONAL:</b> This API is subject to arbitrary change, including renaming or removal.</p>
+ * 
+ * An {@link IListProperty} extension interface with convenience methods for
+ * creating nested {@link EStructuralFeature}s
+ * 
+ * @since 2.5
+ * @noextend This interface is not intended to be extended by clients.
+ * @noimplement This interface is not intended to be implemented by clients.
+ */
+public interface IEMFListProperty extends IEMFProperty, IListProperty
+{
+
+  /**
+   * <p><b>PROVISIONAL:</b> This API is subject to arbitrary change, including renaming or removal.</p>
+   * <p><b>This API is highly experimental and if possible will get moved upwards to Core-Databinding</b></p>
+   * 
+   * Delegate used to read/write an element of an observed list
+   * 
+   * @since 2.7
+   */
+  public abstract class ListElementAccess<O>
+  {
+    /**
+     * <p><b>PROVISIONAL:</b> This API is subject to arbitrary change, including renaming or removal.</p>
+     * <p><b>This API is highly experimental and if possible will get moved upwards to Core-Databinding</b></p>
+     * 
+     * Struct to hold write information
+     * 
+     * @since 2.7
+     */
+    public static class WriteData
+    {
+      /**
+       * If the value should be appended
+       */
+      public static final int NO_INDEX = -1;
+
+      /**
+       * The index in the list the value is written to
+       */
+      public final int index;
+
+      /**
+       * <code>true</code> if the given value should be inserted at the given position
+       */
+      public final boolean insert;
+
+      /**
+       * Create new instance
+       * @param index The index in the list the value is written to
+       * @param insert <code>true</code> if the given value should be inserted at the given position
+       */
+      public WriteData(int index, boolean insert)
+      {
+        this.index = index;
+        this.insert = insert;
+      }
+    }
+
+    /**
+     * The index in the list the value is read from or {@link WriteData#NO_INDEX} if not available
+     * @param list the list to search in
+     * @return the index or <code>{@link WriteData#NO_INDEX}</code> if not found
+     */
+    public abstract int getReadValueIndex(List< O > list);
+
+    /**
+     * The index in the list the value is written to or {@link WriteData#NO_INDEX} if appended
+     * @param list the list the value will is written in
+     * @return The index in the list the value is written to or {@link WriteData#NO_INDEX} if appended
+     */
+    public abstract int getWriteValueIndex(List< O > list);
+
+    /**
+     * The data used to add/replace when writing the value back to the list
+     * @param list the list the value will is written in
+     * @return the data
+     */
+    public WriteData getWriteValueData(List< O > list)
+    {
+      int idx = getWriteValueIndex(list);
+      return new WriteData(idx, idx == WriteData.NO_INDEX);
+    }
+  }
+
+  /**
+   * Returns a master-detail combination of this property and the specified
+   * value nested feature.
+   * 
+   * @param featurePath
+   *            the nested feature to observe
+   * @return a nested combination of this property and the specified nested
+   *         feature.
+   * @see #values(IEMFValueProperty)
+   */
+  public IEMFListProperty values(FeaturePath featurePath);
+
+  /**
+   * Returns a master-detail combination of this property and the specified
+   * value property.
+   * 
+   * @param feature
+   *            the feature
+   * @return a nested combination of this property and the specified nested
+   *         feature.
+   */
+  public IEMFListProperty values(EStructuralFeature feature);
+
+  /**
+   * Returns a master-detail combination of this property and the specified
+   * value property. The returned property will observe the specified value
+   * property for all elements observed by this list property.
+   * <p>
+   * Example:
+   * 
+   * <pre>
+   * // Observes the list-typed &quot;children&quot; property of a Person object,
+   * // where the elements are Person objects
+   * IEMFListProperty children = EMFProperties
+   * 		.list(MyPackage.Literals.PERSON_CHILDREN);
+   * // Observes the string-typed &quot;name&quot; property of a Person object
+   * IEMFValueProperty name = EMFProperties.value(MyPackage.Literals.PERSON_NAME);
+   * // Observes the names of children of a Person object.
+   * IEMFListProperty childrenNames = children.values(name);
+   * </pre>
+   * 
+   * @param property
+   *            the detail property to observe
+   * @return a master-detail combination of this property and the specified
+   *         value property.
+   */
+  public IEMFListProperty values(IEMFValueProperty property);
+
+  /**
+   * <p><b>This API is highly experimental and if possible will get moved upwards to Core-Databinding</b></p>
+   * Observe one of the elements in the list
+   * @param elementAccess the element access to use
+   * @return a master-detail property
+   * @since 2.7
+   */
+  public IEMFValueProperty value(ListElementAccess<?> elementAccess);
+}
---
+++ org.eclipse.emf.databinding/src/org/eclipse/emf/databinding/internal/EMFPropertyListener.java
@@ -0,0 +1,361 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2009 BestSolution.at and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Tom Schindl <tom.schindl@bestsolution.at> - port to EMF in 262160
+ * </copyright>
+ *
+ * $Id: EMFPropertyListener.java,v 1.7 2011/02/08 22:28:34 tschindl Exp $
+ */
+package org.eclipse.emf.databinding.internal;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import org.eclipse.core.databinding.observable.Diffs;
+import org.eclipse.core.databinding.observable.list.ListDiff;
+import org.eclipse.core.databinding.observable.list.ListDiffEntry;
+import org.eclipse.core.databinding.observable.map.MapDiff;
+import org.eclipse.core.databinding.observable.set.SetDiff;
+import org.eclipse.core.databinding.property.INativePropertyListener;
+import org.eclipse.core.databinding.property.IProperty;
+import org.eclipse.core.databinding.property.ISimplePropertyListener;
+import org.eclipse.core.databinding.property.SimplePropertyEvent;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.Notifier;
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.common.util.EMap;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+
+/**
+ * <p><b>PROVISIONAL:</b> This API is subject to arbitrary change, including renaming or removal.</p>
+ *
+ * @since 2.5
+ */
+public abstract class EMFPropertyListener extends AdapterImpl implements INativePropertyListener
+{
+
+  public void addTo(Object source)
+  {
+    if (source != null)
+      ((EObject)source).eAdapters().add(this);
+  }
+
+  public void removeFrom(Object source)
+  {
+    if (source != null)
+      ((EObject)source).eAdapters().remove(this);
+  }
+
+  @Override
+  public abstract void notifyChanged(Notification msg);
+
+  /**
+   * @return the listener
+   */
+  protected abstract ISimplePropertyListener getListener();
+
+  /**
+   * @return the feature
+   */
+  protected abstract EStructuralFeature getFeature();
+
+  /**
+   * @return the owner property
+   */
+  protected abstract IProperty getOwner();
+
+  /**
+   *
+   */
+  public abstract static class EMFListPropertyListener extends EMFPropertyListener
+  {
+    @Override
+    public void notifyChanged(Notification msg)
+    {
+      if (getFeature() == msg.getFeature() && !msg.isTouch())
+      {
+        final ListDiff diff;
+        switch (msg.getEventType())
+        {
+          case Notification.ADD: {
+            diff = Diffs.createListDiff(Diffs.createListDiffEntry(msg.getPosition(), true, msg.getNewValue()));
+            break;
+          }
+          case Notification.ADD_MANY: {
+            Collection< ? > newValues = (Collection< ? >)msg.getNewValue();
+            ListDiffEntry[] listDiffEntries = new ListDiffEntry [newValues.size()];
+            int position = msg.getPosition();
+            int index = 0;
+            for (Object newValue : newValues)
+            {
+              listDiffEntries[index++] = Diffs.createListDiffEntry(position++, true, newValue);
+            }
+            diff = Diffs.createListDiff(listDiffEntries);
+            break;
+          }
+          case Notification.REMOVE: {
+            diff = Diffs.createListDiff(Diffs.createListDiffEntry(msg.getPosition(), false, msg.getOldValue()));
+            break;
+          }
+          case Notification.REMOVE_MANY: {
+            Collection< ? > oldValues = (Collection< ? >)msg.getOldValue();
+            ListDiffEntry[] listDiffEntries = new ListDiffEntry [oldValues.size()];
+            int[] positions = (int[])msg.getNewValue();
+            if (positions == null)
+            {
+              int index = 0;
+              for (Object oldValue : oldValues)
+              {
+                listDiffEntries[index] = Diffs.createListDiffEntry(0, false, oldValue);
+                ++index;
+              }
+            }
+            else
+            {
+              int index = 0;
+              for (Object oldValue : oldValues)
+              {
+                listDiffEntries[index] = Diffs.createListDiffEntry(positions[index] - index, false, oldValue);
+                ++index;
+              }
+            }
+            diff = Diffs.createListDiff(listDiffEntries);
+            break;
+          }
+          case Notification.SET:
+          case Notification.RESOLVE: {
+            ListDiffEntry[] listDiffEntries = new ListDiffEntry [2];
+            int pos = msg.getPosition();
+            // Looks like a single valued feature
+            if (pos == -1)
+            {
+              pos = 0;
+            }
+            listDiffEntries[0] = Diffs.createListDiffEntry(pos, false, msg.getOldValue());
+            listDiffEntries[1] = Diffs.createListDiffEntry(pos, true, msg.getNewValue());
+            diff = Diffs.createListDiff(listDiffEntries);
+            break;
+          }
+          case Notification.MOVE: {
+            Object movedValue = msg.getNewValue();
+            ListDiffEntry[] listDiffEntries = new ListDiffEntry [2];
+            listDiffEntries[0] = Diffs.createListDiffEntry((Integer)msg.getOldValue(), false, movedValue);
+            listDiffEntries[1] = Diffs.createListDiffEntry(msg.getPosition(), true, movedValue);
+            diff = Diffs.createListDiff(listDiffEntries);
+            break;
+          }
+          case Notification.UNSET: {
+            // This just represents going back to the unset state, but
+            // that doesn't affect the contents of the list.
+            //
+            return;
+          }
+          default: {
+            throw new RuntimeException("unhandled case");
+          }
+        }
+        getListener().handleEvent((new SimplePropertyEvent(SimplePropertyEvent.CHANGE, msg.getNotifier(), getOwner(), diff)));
+      }
+    }
+  }
+
+  /**
+   *
+   */
+  public abstract static class EMFSetPropertyListener extends EMFPropertyListener
+  {
+    @Override
+    public void notifyChanged(Notification msg)
+    {
+      if (getFeature() == msg.getFeature() && !msg.isTouch())
+      {
+        final SetDiff diff;
+        switch (msg.getEventType())
+        {
+          case Notification.ADD: {
+            diff = Diffs.createSetDiff(Collections.singleton(msg.getNewValue()), Collections.emptySet());
+            break;
+          }
+          case Notification.ADD_MANY: {
+            Collection< ? > newValues = (Collection< ? >)msg.getNewValue();
+            diff = Diffs.createSetDiff(new HashSet<Object>(newValues), Collections.emptySet());
+            break;
+          }
+          case Notification.REMOVE: {
+            diff = Diffs.createSetDiff(Collections.emptySet(), Collections.singleton(msg.getOldValue()));
+            break;
+          }
+          case Notification.REMOVE_MANY: {
+            Collection< ? > oldValues = (Collection< ? >)msg.getOldValue();
+            diff = Diffs.createSetDiff(Collections.emptySet(), new HashSet<Object>(oldValues));
+            break;
+          }
+          case Notification.SET:
+          case Notification.RESOLVE: {
+            diff = Diffs.createSetDiff(Collections.singleton(msg.getNewValue()), Collections.singleton(msg.getOldValue()));
+            break;
+          }
+          case Notification.MOVE:
+          case Notification.UNSET: {
+            // This just represents going back to the unset state, but
+            // that doesn't affect the contents of the list.
+            //
+            return;
+          }
+          default: {
+            throw new RuntimeException("unhandled case");
+          }
+        }
+        getListener().handleEvent((new SimplePropertyEvent(SimplePropertyEvent.CHANGE, msg.getNotifier(), getOwner(), diff)));
+      }
+    }
+  }
+
+  /**
+   *
+   */
+  public abstract static class EMFMapPropertyListener extends EMFPropertyListener
+  {
+		@Override
+		public void addTo(Object source)
+		{
+			super.addTo(source);
+
+			if (source != null)
+			{
+				EMap<?, ?> map = (EMap<?, ?>) ((EObject) source).eGet(getFeature());
+
+				for (Map.Entry<?, ?> entry : map)
+				{
+					((Notifier) entry).eAdapters().add(this);
+				}
+			}
+		}
+
+		@Override
+		public void removeFrom(Object source)
+		{
+			if (source != null)
+			{
+				EMap<?, ?> map = (EMap<?, ?>) ((EObject) source).eGet(getFeature());
+
+				for (Map.Entry<?, ?> entry : map)
+				{
+					((Notifier) entry).eAdapters().remove(this);
+				}
+			}
+
+			super.removeFrom(source);
+		}
+	  
+    @Override
+    public void notifyChanged(Notification msg)
+    {
+      if (msg.isTouch())
+    	return;
+      
+      if (getFeature() == msg.getFeature())
+      {
+        final MapDiff diff;
+        switch (msg.getEventType())
+        {
+          case Notification.ADD: {
+            Map.Entry<?, ?> entry = (Map.Entry<?, ?>)msg.getNewValue();
+            ((EObject) entry).eAdapters().add(this);
+            diff = Diffs.createMapDiffSingleAdd(entry.getKey(), entry.getValue());
+            break;
+          }
+          case Notification.ADD_MANY: {
+            @SuppressWarnings("unchecked")
+            Collection<Map.Entry<?, ?>> newEntries = (Collection<Map.Entry<?, ?>>)msg.getNewValue();
+            Map<Object, Object> newValues = new HashMap<Object, Object>();
+            for (Map.Entry<?, ?> entry : newEntries)
+            {
+              ((EObject) entry).eAdapters().add(this);
+              newValues.put(entry.getKey(), entry.getValue());
+            }
+            diff = Diffs.createMapDiff(newValues.keySet(), Collections.emptySet(), Collections.emptySet(), Collections.emptyMap(), newValues);
+            break;
+          }
+          case Notification.REMOVE: {
+            Map.Entry<?, ?> entry = (Map.Entry<?, ?>)msg.getOldValue();
+            ((EObject) entry).eAdapters().remove(this);
+            diff = Diffs.createMapDiffSingleRemove(entry.getKey(), entry.getValue());
+            break;
+          }
+          case Notification.REMOVE_MANY: {
+            @SuppressWarnings("unchecked")
+            Collection<Map.Entry<?, ?>> oldEntries = (Collection<Map.Entry<?, ?>>)msg.getOldValue();
+            Map<Object, Object> oldValues = new HashMap<Object, Object>();
+            for (Map.Entry<?, ?> entry : oldEntries)
+            {
+              ((EObject) entry).eAdapters().remove(this);
+              oldValues.put(entry.getKey(), entry.getValue());
+            }
+            diff = Diffs.createMapDiff(Collections.emptySet(), oldValues.keySet(), Collections.emptySet(), oldValues, Collections.emptyMap());
+            break;
+          }
+          case Notification.SET:
+          case Notification.RESOLVE: {
+            Map.Entry<?, ?> oldEntry = (Map.Entry<?, ?>)msg.getOldValue();
+            ((EObject) oldEntry).eAdapters().remove(this);
+            Map<Object, Object> oldValues = new HashMap<Object, Object>();
+            oldValues.put(oldEntry.getKey(), oldEntry.getValue());
+            Map.Entry<?, ?> newEntry = (Map.Entry<?, ?>)msg.getNewValue();
+            ((EObject) newEntry).eAdapters().add(this);
+            Map<Object, Object> newValues = new HashMap<Object, Object>();
+            newValues.put(newEntry.getKey(), newEntry.getValue());
+            diff = Diffs.createMapDiff(newValues.keySet(), oldValues.keySet(), Collections.emptySet(), oldValues, newValues);
+            break;
+          }
+          case Notification.MOVE:
+          case Notification.UNSET: {
+            // This just represents going back to the unset state, but
+            // that doesn't affect the contents of the list.
+            //
+            return;
+          }
+          default: {
+            throw new RuntimeException("unhandled case");
+          }
+        }
+        getListener().handleEvent((new SimplePropertyEvent(SimplePropertyEvent.CHANGE, msg.getNotifier(), getOwner(), diff)));
+      } else if (((EClass) getFeature().getEType()).getEStructuralFeature("value") == msg.getFeature()) {
+      	Object key = ((Map.Entry<?, ?>) msg.getNotifier()).getKey();
+		MapDiff diff = Diffs.createMapDiffSingleChange(key, msg.getOldValue(), msg.getNewValue());
+      	getListener().handleEvent(new SimplePropertyEvent(SimplePropertyEvent.CHANGE, msg.getNotifier(), getOwner(), diff));
+      }
+    }
+  }
+
+  /**
+   *
+   */
+  public abstract static class EMFValuePropertyListener extends EMFPropertyListener
+  {
+    @Override
+    public void notifyChanged(Notification msg)
+    {
+      if (getFeature() == msg.getFeature() && !msg.isTouch())
+      {
+        getListener().handleEvent(
+          new SimplePropertyEvent(SimplePropertyEvent.CHANGE, msg.getNotifier(), getOwner(), Diffs.createValueDiff(
+            msg.getOldValue(),
+            msg.getNewValue())));
+      }
+    }
+  }
+}
