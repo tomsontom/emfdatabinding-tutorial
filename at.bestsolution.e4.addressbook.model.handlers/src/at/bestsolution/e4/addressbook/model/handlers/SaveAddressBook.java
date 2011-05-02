1aab5498caa04b3a5a01e8ccbfb939dc6882bbe3
Tom Schindl <tom.schindl@bestsolution.at>
2011-05-02 07:00:48 +0200

    fixed copyright

--- at.bestsolution.e4.addressbook.model.handlers/src/at/bestsolution/e4/addressbook/model/handlers/SaveAddressBook.java
+++ at.bestsolution.e4.addressbook.model.handlers/src/at/bestsolution/e4/addressbook/model/handlers/SaveAddressBook.java
@@ -1,19 +1,13 @@
-/**
- * <copyright>
- *
- * Copyright (c) 2005 IBM Corporation and others.
- * All rights reserved.   This program and the accompanying materials
- * are made available under the terms of the Common Public License v1.0
+/*******************************************************************************
+ * Copyright (c) 2011, BestSolution.at and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
- * 
- * Contributors: 
- *   IBM - Initial API and implementation
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * </copyright>
- *
- * $Id: EMF_codetemplates.xml,v 1.1 2008/05/06 20:12:20 nickb Exp $
- */
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - Initial API and implementation
+ *******************************************************************************/
 package at.bestsolution.e4.addressbook.model.handlers;
 
 import java.io.IOException;
--- at.bestsolution.e4.ui.workbench.renderers.swing/src/at/bestsolution/e4/ui/workbench/renderers/swing/ToolItemRenderer.java
+++ at.bestsolution.e4.ui.workbench.renderers.swing/src/at/bestsolution/e4/ui/workbench/renderers/swing/ToolItemRenderer.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011, BestSolution.at and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl <tom.schindl@bestsolution.at> - Initial API and implementation
+ *******************************************************************************/
 package at.bestsolution.e4.ui.workbench.renderers.swing;
 
 import java.awt.event.MouseAdapter;
